d3c0ae5d353f8758e54b024fbbdcb27b52ec8668
Tri Tam HOANG <tritam.hoang@gmail.com>
2020-02-29 16:55:29 +0100

    Add logger, implement frame methods.

---
+++ src/main/java/mc8max/tool/jsc/Logger.java
@@ -0,0 +1,30 @@
+package mc8max.tool.jsc;
+
+public class Logger 
+{
+	private Logger()
+	{
+		// private, empty on purpose
+	}
+	
+	public static void info(String msg)
+	{
+		System.out.println(msg);
+	}
+	
+	public static void debug(String msg)
+	{
+		System.out.println(msg);
+	}
+	
+	public static void error(String msg)
+	{
+		System.out.println(msg);
+	}
+	
+	public static void fail(String msg)
+	{
+		System.out.println(msg);
+		System.exit(-1);
+	}
+}
---
+++ src/main/java/mc8max/tool/jsc/analyzer/StackAnalyzer.java
@@ -0,0 +1,21 @@
+package mc8max.tool.jsc.analyzer;
+
+import java.io.File;
+
+public class StackAnalyzer 
+{
+	public StackAnalyzer(File file)
+	{
+		
+	}
+
+	public void compute()
+	{
+		
+	}
+	
+	public void display()
+	{
+		
+	}
+}
